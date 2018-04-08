c90fb0c633192b756369b9d8fe749c57ec399969
Unknown <guildhunt@knou.ac.kr>
2018-04-08 15:23:31 +0900

    클래스 분류 완료

--- chapter4/bayes_01_init.m
+++ chapter4/bayes_01_init.m
@@ -2,18 +2,25 @@
 clear;
 N = 100;
 
-m1=repmat([0,0], N, 1);
-S1= [1 0 ; 0 1];
-X1=randn(N, 2) * sqrtm(S1) + m1;
+m3=repmat([0, 3.5], N, 1);
+S3= [2 0 ; 0 1];
+X3=randn(N, 2) * sqrtm(S3) + m3;
+
 
 m2=repmat([3,3], N, 1);
 S2= [1 1.6 ; 1.6 4];
 X2=randn(N, 2) * sqrtm(S2) + m2;
 
-m3=repmat([0, 3.5], N, 1);
-S3= [2 0 ; 0 1];
-X3=randn(N, 2) * sqrtm(S3) + m3;
 
+m1=repmat([0,0], N, 1);
+S1= [1 0 ; 0 1];
+X1=randn(N, 2) * sqrtm(S1) + m1;
+
+
+
+mm1 = mean(X1)
+mm2 = mean(X2)
+mm3 = mean(X3)
 
 
 plot(X1(:, 1), X1(:, 2), '.');
--- chapter4/bayes_02_Classifier.m
+++ chapter4/bayes_02_Classifier.m
@@ -7,7 +7,8 @@ mm1 = mean(X1);
 mm2 = mean(X2);
 mm3 = mean(X3);
 
-M=[ mean(X1):mean(X2):mean(X3)]; 
+% avg Mean for per class
+M=[ mean(X1);mean(X2);mean(X3)]; 
 
 S(:,:,1) = cov(X1);
 S(:,:,2) = cov(X2);
@@ -15,7 +16,7 @@ S(:,:,3) = cov(X3);
 
 
 smean=(cov(X1) + cov(X2) + cov(X3)) / 3 ;
-Dtrain=[X1:X2:X3];   
+Dtrain=[X1;X2;X3];   
 
 Etrain=zeros(3,1);
 N = size(X1, 1);
@@ -24,10 +25,11 @@ for k=1:K
     X = Dtrain((k-1) * 100+1:k*100,:);
     for i=1:N
         for j=1:K
-            d1(j,1)=(X(i,:)-M(j,:)) * (X(i,:)-M(j,:))';
-            d2(j,1)=(X(i,:)-M(j,:)) * inv(smean) * (X(i,:)-M(j,:))';
-            d3(j,1)=(X(i,:)-M(j,:)) * inv(reshape(S(:,:,j),2,2)) * (X(i,:)-M(j,:))';
+            d1(j,1) = (X(i,:)-M(j,:)) * (X(i,:)-M(j,:))';
+            d2(j,1) = (X(i,:)-M(j,:)) * inv(smean) * (X(i,:)-M(j,:))';
+            d3(j,1) = (X(i,:)-M(j,:)) * inv(reshape(S(:,:,j),2,2)) * (X(i,:)-M(j,:))';
         end
+        
         [min1v, min1i] = min(d1);
         if(min1i~=k) 
             Etrain(1,1) = Etrain(1,1)+1; 
@@ -45,5 +47,5 @@ for k=1:K
     end
 end
 
-Error_rate = ETrain/(N*K)
+Error_rate = Etrain/(N*K)
    
