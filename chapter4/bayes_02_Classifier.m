8598e99428e54d4217ef84713d33fbe145a696bd
guild <guildhunt@knou.ac.kr>
2018-04-21 12:32:36 +0900

    commit learn

--- chapter4/bayes_02_Classifier.m
+++ chapter4/bayes_02_Classifier.m
@@ -1,64 +1,49 @@
 clear;
 
-KlassCount=3;
-%load data4_1.mat 
-load dataCh4_7
+K=3;
+load data4_1.mat 
 
+mm1 = mean(X1);
+mm2 = mean(X2);
+mm3 = mean(X3);
 
-% avg Mean for per class
-meanArray=[ mean(X1);mean(X2);mean(X3)]; 
+M=[ mean(X1):mean(X2):mean(X3)]; 
 
 S(:,:,1) = cov(X1);
 S(:,:,2) = cov(X2);
 S(:,:,3) = cov(X3);
 
+
 smean=(cov(X1) + cov(X2) + cov(X3)) / 3 ;
-trainData=[X1;X2;X3];   
+Dtrain=[X1:X2:X3];   
 
-trainErr=zeros(3,1);
-DataArrCount = size(X1, 1);
+Etrain=zeros(3,1);
+N = size(X1, 1);
 
-for klassIndex=1:KlassCount
-    dataSet = trainData((klassIndex-1) * DataArrCount+1:klassIndex*DataArrCount,:);
-    for i=1:DataArrCount
-        for j=1:3       % 세개의 판별함수 값 계산
-            
-            % d1(첫번째 판별함수 저장) = (c1 첫번째 데이터 - c1 첫번째 평균) * (c1 첫번째 데이터 - c1
-            % 첫번째 평균)
-            d1(j,1) = (dataSet(i,:)-meanArray(j,:)) * (dataSet(i,:)-meanArray(j,:))';
-            d2(j,1) = (dataSet(i,:)-meanArray(j,:)) * inv(smean) * (dataSet(i,:)-meanArray(j,:))';
-            
-            % d1(첫번째 판별함수 저장) = (c1 첫번째 데이터 - c1 첫번째 평균) * (재계산된 공분산 * 데이터 )
-            d3(j,1) = (dataSet(i,:)-meanArray(j,:)) * inv(reshape(S(:,:,j),2,2)) * (dataSet(i,:)-meanArray(j,:))';
+for k=1:K
+    X = Dtrain((k-1) * 100+1:k*100,:);
+    for i=1:N
+        for j=1:K
+            d1(j,1)=(X(i,:)-M(j,:)) * (X(i,:)-M(j,:))';
+            d2(j,1)=(X(i,:)-M(j,:)) * inv(smean) * (X(i,:)-M(j,:))';
+            d3(j,1)=(X(i,:)-M(j,:)) * inv(reshape(S(:,:,j),2,2)) * (X(i,:)-M(j,:))';
         end
-        
-        
-        %min은 최솟값 v와 인덱스 v를 알려줍니다
         [min1v, min1i] = min(d1);
-        
-        % 결정함수에서 나온 최솟값과 클래스의 타입이 다르면 오류
-        % 벗어낫다고 합니다
-        if(min1i~=klassIndex) 
-            trainErr(1,1) = trainErr(1,1)+1; 
+        if(min1i~=k) 
+            Etrain(1,1) = Etrain(1,1)+1; 
         end
-        
         
         [min2v, min2i] = min(d2);
-        if(min2i~=klassIndex) 
-            trainErr(2,1) = trainErr(2,1)+1; 
+        if(min2i~=k) 
+            Etrain(2,1) = Etrain(2,1)+1; 
         end
         
         [min3v, min3i] = min(d3);
-        if(min3i~=klassIndex) 
-            trainErr(3,1) = trainErr(3,1)+1; 
+        if(min3i~=k) 
+            Etrain(3,1) = Etrain(3,1)+1; 
         end
-        
     end
 end
 
-
-
-
-
-Error_rate = trainErr/(DataArrCount*KlassCount);
+Error_rate = ETrain/(N*K)
    
